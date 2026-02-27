fa85af57be29a4c8040c0e5627b981b17edb9225
24331a05e3-cell <24331a05e3@mvgrce.edu.in>
2026-02-27 07:17:56 +0530

    Add files via upload

---
+++ 8-3.py
@@ -0,0 +1,10 @@
+#24331A05E3
+#To implement the dictionary methods 
+dct = {'numbers': [1,2,3,4,5]}
+print(dct.keys())
+print(dct.values())
+print(dct.items())
+dct['numbers'].remove(5)
+print(dct)
+dct.pop('numbers')
+print(dct)
